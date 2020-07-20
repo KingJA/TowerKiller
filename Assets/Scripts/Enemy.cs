636cb03613220b276f132ebe82c5abf5f5d1f4ed
KingJA <kingjavip@gmail.com>
2020-07-20 17:52:00 +0800

    5

---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour {
+    public float speed = 10;
+    private Transform[] positions;
+    private int index = 0;
+
+    void Start(){
+        positions = WayPoints.positions;
+    }
+
+    // Update is called once per frame
+    void Update(){
+        Move();
+    }
+
+    void Move(){
+        /*移动到最后一个节点*/
+        if (index > positions.Length - 1) {
+            return;
+        }
+        /*开始移动*/
+        transform.Translate((positions[index].position - transform.position).normalized * Time.deltaTime * speed);
+        /*移动到下个节点*/
+        if (Vector3.Distance(positions[index].position, transform.position) < 0.2) {
+            index++;
+        }
+
+        /*到达终点，销毁*/
+        if (index > positions.Length - 1) {
+            ReachDestination();
+        }
+    }
+
+    void ReachDestination(){
+        GameObject.Destroy(this.gameObject);
+    }
+
+    private void OnDestroy(){
+        EnemySpawner.CountEnemyAlive--;
+    }
+}
---
+++ Assets/Scripts/EnemySpawner.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemySpawner : MonoBehaviour {
+    public Wave[] waves;
+    public Transform start;
+    public static int CountEnemyAlive = 0;
+    public float waveRate = 0.5f;
+
+    // Start is called before the first frame update
+    void Start(){
+        StartCoroutine(SpawnEnemy());
+    }
+
+    IEnumerator SpawnEnemy(){
+        foreach (Wave wave in waves) {
+            for (int i = 0; i < wave.count; i++) {
+                GameObject.Instantiate(wave.enemyPrefab, start.position, Quaternion.identity);
+                CountEnemyAlive++;
+                if (i != wave.count - 1) {
+                    yield return new WaitForSeconds(wave.rate);
+                }
+            }
+
+            while (CountEnemyAlive > 0) {
+                /*暂停*/
+                yield return 0;
+            }
+
+            yield return new WaitForSeconds(waveRate);
+        }
+    }
+
+    // Update is called once per frame
+    void Update(){ }
+}
